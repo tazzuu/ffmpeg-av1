704fc1d5297a6d7c5d69b72d5cc658813e84d52f
tazzuu <tazzuu@users.noreply.github.com>
2025-03-12 01:15:39 -0400

    fix ffmpeg_svt-av1.sh script

--- scripts/ffmpeg_svt-av1.sh
+++ scripts/ffmpeg_svt-av1.sh
@@ -6,13 +6,15 @@ OUTPUT_FILE="$2"
 VIDEO_CODEC="libsvtav1"
 CRF=25
 PRESET=4
+INPUT_DIR="$(dirname "$INPUT_FILE")"
 
 CONTAINER=ffmpeg-av1:7.1.1
-FFMPEG_BIN="docker run -v $PWD:$PWD --workdir $PWD $CONTAINER ffmpeg"
-FFPROBE_BIN="docker run -v $PWD:$PWD --workdir $PWD $CONTAINER ffprobe"
+FFMPEG_BIN="docker run -v $PWD:$PWD --workdir $PWD -v '$INPUT_DIR':'$INPUT_DIR' '$CONTAINER' ffmpeg"
+FFPROBE_BIN="docker run -v $PWD:$PWD --workdir $PWD -v '$INPUT_DIR':'$INPUT_DIR' '$CONTAINER' ffprobe"
+FFPROBE_CMD="$FFPROBE_BIN -v error -select_streams v -show_entries stream=index -of csv=p=0 '$INPUT_FILE'"
 
 # Get video stream index
-VIDEO_STREAM=$($FFPROBE_BIN -v error -select_streams v -show_entries stream=index -of csv=p=0 "$INPUT_FILE")
+VIDEO_STREAM=$(eval $FFPROBE_CMD)
 
 # Build FFmpeg command
 FFMPEG_CMD="$FFMPEG_BIN -y -i \"$INPUT_FILE\" -pix_fmt yuv420p10le -g 240 -svtav1-params tune=0"
@@ -21,7 +23,8 @@ FFMPEG_CMD="$FFMPEG_BIN -y -i \"$INPUT_FILE\" -pix_fmt yuv420p10le -g 240 -svtav
 FFMPEG_CMD+=" -map 0:$VIDEO_STREAM -c:v $VIDEO_CODEC -crf $CRF -preset $PRESET"
 
 # Map and copy all non-video streams (audio, subtitles, etc.)
-STREAMS=$($FFPROBE_BIN -v error -show_entries stream=index,codec_type -of csv=p=0 "$INPUT_FILE")
+STREAMS_CMD="$FFPROBE_BIN -v error -show_entries stream=index,codec_type -of csv=p=0 '$INPUT_FILE'"
+STREAMS=$(eval $STREAMS_CMD)
 
 while IFS=, read -r INDEX TYPE; do
   if [[ "$TYPE" != "video" ]]; then
