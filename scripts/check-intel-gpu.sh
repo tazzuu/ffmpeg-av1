a42ecd12209685d41ce25ef3aededf21c630c479
tazzuu <tazzuu@users.noreply.github.com>
2025-04-15 15:11:12 +0000

    fix intel check script

--- scripts/check-intel-gpu.sh
+++ scripts/check-intel-gpu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
+CONTAINER="ffmpeg-av1:7.1.1-intel"
 # get the path to the GPU for Intel ; this can change on reboot
 INTEL_PCI_NODE="$(lspci | grep 'VGA compatible controller: Intel Corporation' | cut -d ' ' -f1)"
 INTEL_CARD="$(readlink -f /dev/dri/by-path/pci-0000:$INTEL_PCI_NODE-card)"
@@ -14,7 +15,8 @@ docker run --rm \
 -e LIBVA_DRIVER_NAME=iHD \
 -e LIBVA_DRIVERS_PATH=/usr/lib/x86_64-linux-gnu/dri \
 -e XDG_RUNTIME_DIR=/tmp \
-ffmpeg-av1:7.1.1 vainfo --display drm --device $INTEL_RENDER
+"${CONTAINER}" \
+vainfo --display drm --device $INTEL_RENDER
 
 # check for OpenCL
-docker run --rm --device=/dev/dri:/dev/dri --group-add video ffmpeg-av1:7.1.1 clinfo
+docker run --rm --device=/dev/dri:/dev/dri --group-add video "${CONTAINER}" clinfo
